e2aafa5c51721c63c55c1caf9466ece54d6c1675
soldatsm <soldatsm@yandex.ru>
2023-09-28 17:17:10 +0300

    first commitment

---
+++ dbCAN_annotator.py
@@ -0,0 +1,17 @@
+import argparse
+import tqdm
+import os
+import subprocess
+from multiprocessing import Pool
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-input')
+
+args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    pass
+
