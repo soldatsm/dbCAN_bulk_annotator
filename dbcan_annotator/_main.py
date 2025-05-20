08426d0e8ebc35b8c422daa24d776432000cbb82
soldatsm <soldatsm@yandex.ru>
2025-05-20 16:02:54 +0300

    Add some more statistics before running script. Add ability to abort script before it starts.

--- dbcan_annotator/_main.py
+++ dbcan_annotator/_main.py
@@ -3,7 +3,8 @@ import os
 from datetime import datetime
 from multiprocessing import Pool
 import shutil
-from . import annotator, version_printer
+import sys
+from . import annotator, version_printer, folder_checker
 
 def argument_parser() -> argparse.ArgumentParser:
     """
@@ -19,7 +20,8 @@ def argument_parser() -> argparse.ArgumentParser:
                         required=True,
                         type=str)
     parser.add_argument('-p','--pools',
-                        help='Lenght of average sublist. It indicate amount of genomes that will be annotated in one process. More number, faster work, and higher demand on the CPU',
+                        help='Lenght of average sublist. It indicate amount of genomes that will be annotated in one process. More number, faster work, and higher demand on the CPU \
+                        EXAMPLE: you have 50 genomes/proteomes, if you chose -p 10, than it will run 50/10 = 5 run_dbcan scripts in PARALLEL. Each script will annotate 10 genomes/proteomes. IMPORTANT: EACH of run_dbcan will consume number of cpus, set by --cpus option.',
                         default=1,
                         type=int)
     parser.add_argument('-o','--output',
@@ -31,8 +33,8 @@ def argument_parser() -> argparse.ArgumentParser:
                         required=True,
                         type=str)
     parser.add_argument('--cpus',
-                        help='Choose numper of cpus for dbCAN sub programms (dbCAN_sub, HMMER). \
-                            Default is 1s',
+                        help='Choose numper of cpus/threads for dbCAN sub programms (e.g.dbCAN_sub, HMMER). \
+                        Default is 1.',
                         default=1,
                         type=int)
     parser.add_argument('--type', help='Chose input data type [protein, prok, meta]', 
@@ -40,7 +42,7 @@ def argument_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def main():
+def main() -> None:
     """
     Main function for dbCAN bulk annotator.
     
@@ -52,6 +54,7 @@ def main():
     parser = argument_parser()
     args = parser.parse_args()
 
+    #print version of the script
     version_printer(terminal_size=shutil.get_terminal_size())
     
     terminal_size = shutil.get_terminal_size()
@@ -59,16 +62,40 @@ def main():
 
     abs_path_output = os.path.abspath(args.output)
     abs_path_input = os.path.abspath(args.input)
-
     dir_list = os.listdir(abs_path_input)
+
+    #check if output folder is empty
+    if not folder_checker(abs_path_output):
+        print('There is some files in chose folder.')
+        print('Please remove them and run script again')
+        print('Exiting...')
+        print('-' * terminal_size[0])
+        sys.exit()
+
+    #stats
+    SEQ_NUMBER = len(dir_list)
+    PARALLEL = int(SEQ_NUMBER/args.pools)
 
     dir_list = [(f"{abs_path_input}/{i.replace('.faa', '')}",
                  args.dbCAN_database, args.cpus,
                  abs_path_output, terminal_size, args.type)
                 for i in dir_list]
+    
     #creation of nested list for pools
     nested_list = [dir_list[i:i+args.pools]
                    for i in range(0, len(dir_list), args.pools)] 
+
+    #Some messages 
+    print(f'You chose {args.pools} number of pools.')
+    print(f'It means each parallel process will has {args.pools} genomes/proteomes.')
+    print(f'There will be {PARALLEL} run_dbcan scripts running in parallel.')
+    print(f'And each of the parallel process will consume {args.cpus} threads.')
+    print(f'In total {PARALLEL*args.cpus} threads.')
+    print('Is it appropriate for your system?')
+    proceed_answer = input('Continue ? Y/N: ')
+    print('-' * terminal_size[0])
+    if proceed_answer.lower() != 'y':
+        sys.exit()
 
     with Pool(processes=len(nested_list)) as p:
         #mapping function on genomes in each pool
